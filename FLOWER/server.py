5abbbed110a9a38e9ea0a028c11b75328f476431
hperumalla-oops <144527455+hperumalla-oops@users.noreply.github.com>
2025-05-29 08:03:06 +0530

    Update server.py

--- FLOWER/server.py
+++ FLOWER/server.py
@@ -1,6 +1,8 @@
 import flwr as fl
 import sys
 import numpy as np
+import pandas as pd
+
 from model import create_model
 
 def fit_config(rnd: int):
@@ -8,6 +10,24 @@ def fit_config(rnd: int):
 
 def evaluate_config(rnd: int):
     return {"val_steps": 5}
+
+def load_data(path):
+    df = pd.read_csv(path)
+    X = df.iloc[:, :-1].values
+    y = df.iloc[:, -1].values
+    return X, y
+
+def evaluate_fn(server_round, parameters, config):
+    model = create_model(input_dim=187) ##need to change in the future
+    model.set_weights(parameters)
+
+    x_test, y_test = load_data(r"C:\Users\hperu\OneDrive\Desktop\fl\mitbih_test.csv")
+    loss, acc = model.evaluate(x_test, y_test)
+    return loss, {"accuracy": acc}
+
+
+
+
 
 class SaveModelStrategy(fl.server.strategy.FedAvg):
     def aggregate_fit(
@@ -32,6 +52,7 @@ strategy = fl.server.strategy.FedAvg(
     min_available_clients=2,
     on_fit_config_fn=fit_config,
     on_evaluate_config_fn=evaluate_config,
+    evaluate_fn=evaluate_fn
 )
 
 # Start server
